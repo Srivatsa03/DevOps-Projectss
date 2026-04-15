4c89c9f1ad637b112621876b0f00d4abd1e1b3e8
Srivatsa03 <skamb10@uic.edu>
2026-04-15 13:08:01 -0500

    Run Maven directly in Jenkins pipeline

--- jenkins-cicd-project/src/test/java/com/example/jenkinscicd/HelloControllerTest.java
+++ jenkins-cicd-project/src/test/java/com/example/jenkinscicd/HelloControllerTest.java
@@ -1,26 +1,15 @@
 package com.example.jenkinscicd;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-@AutoConfigureMockMvc
 class HelloControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
     @Test
-    void helloReturnsHelloWorld() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello World"));
+    void helloReturnsHelloWorld() {
+        HelloController controller = new HelloController();
+
+        assertEquals("Hello World", controller.hello());
     }
 }
